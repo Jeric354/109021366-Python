1892164b1602c585401e6bdb5e147e60a4081c4f
Jeric354 <asdasd614004@gmail.com>
2021-04-09 15:31:24 +0800

    some changes

--- demo.py
+++ demo.py
@@ -1,26 +1,14 @@
 import requests
 import bs4
-req = requests.get("http://120.108.116.237/~jackjow/pubList.php")
-req.encoding = 'utf8'
-rc = req.content
-soup = bs4.BeautifulSoup(rc,"html.parser")
+url = "http://isrc.ccs.asia.edu.tw/www/myjournal/myjournal.htm" # 黃明祥老師的連結
+response = requests.get(url)
+response.encoding = 'big5'
+rc = response.content
+soup = bs4.BeautifulSoup(rc, 'html.parser') 
+if response.status_code == 200:
+    f = open('output-publication.txt', 'w', encoding='utf8')
+    for tagP in soup.find_all('p', 'MsoNormal'):
+        t = tagP.text.replace('\t', '').replace('\n', '')
+        f.write(t+'\n')
 
-if req .status_code==200:
-    f = open("output.txt" ,'w' , encoding='utf8')
-    # f.write(soup.find("div", id='text'))
-    for li in soup.find('div' , id=home).find_all('li'):
-        t =li.text.replace('\n','').replace(' ','').replace(' ','')
-        f.write(t+"\n")
-    f.close() 
-
-
-
-# print(soup.body)
-print(soup.find("div" , id="home"))
-print(soup.find("div" , id="home").find_all('li'))
-# print(soup,find("ol" , id="publist").find_all('li'))
-
-#print(req.status_code)
-#print(r.content)
-
-#print(bs4.BeautifulSoup(r.content, "html.parser"))
+f.close()
