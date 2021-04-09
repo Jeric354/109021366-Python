b3811fe373979199b2b4e24edd8a51d0346691d0
Jeric354 <asdasd614004@gmail.com>
2021-04-09 16:02:00 +0800

    jeric is gay

--- wordList.py
+++ wordList.py
@@ -20,28 +20,38 @@ def parse_html(html_str) :
     return BeautifulSoup(html_str, "lxml")
 
 
-if __name__ == "__main__":
-    urlx = generate_urls(URL, 1, 10)
-    for url in urlx :
+
+def     get_word(soup,file):
+    words = []
+    count = 0
+    for wordlist_table in soup.find_all(class_="wordlist"):
+        count +=1
+        for word_entry in wordlist_table.find_all("tr"):
+            new_word = []
+            new_word.append(file)
+            new_word.append(str(count))
+            new_word.append(word_entry.th.text)
+            new_word.append(word_entry.td.text)
+            words.append(new_word)
+    return words
+def wed_scraping_bot(urls):
+    eng_words = []
+    for url in urls:
         file = url.split("/")[-1]
-        print('catching:', file, 'wed data...')
+        print("catching:", file, "wed data...")
         r = get_resource(url)
-        if r.status_code == requests.codes.ok :
+        if r.status_code == requests.code.ok:
             soup = parse_html(r.text)
-            words = []
-            count = 0
-            for wordlist_table in soup.find_all(class_="wordlist"):
-                count +=1
-                for word_entry in wordlist_table.find_all("tr"):
-                    new_word = []
-                    new_word.append(file)
-                    new_word.append(str(count))
-                    new_word.append(word_entry.th.text)
-                    new_word.append(word_entry.td.text)
-                    words.append(new_word)
-            print(*words)
-            print('------\n\n')
+            words = get_word(soup,file)
+            eng_words = eng_words + words
+            print("waiting 5 secound...")
+            time.sleep(5)
         else:
-            print('HTTP request error')
-        print("sleep 5 secound")    
-        time.sleep(5)
+            print('HTTP request error')    
+    return eng_words    
+
+
+    if __name__ == "__main__":
+    urlx = generate_urls(URL, 1, 3)
+    eng_words = wed_scraping_bot(urlx
+    print(eng_words)
