c3e89edce5188533310e182ccb9b0f4e53d6dc1e
Jeric354 <80087289+Jeric354@users.noreply.github.com>
2021-06-20 16:53:21 +0800

    Add files via upload

---
+++ 期末程式/0306-2.py
@@ -0,0 +1,19 @@
+  
+class Chess:
+    def __init__(self, word ,color ,play):
+        self.word1  =word
+        self.color1 =color
+        self.play1 = play
+
+    def showInfo(self):
+        print(self.word1)
+        print(self.color1)
+        print(self.play1)
+
+x = Chess("象","red","前後左右吃")
+y = Chess("炮","red","隔著吃")    
+z = Chess("帥","red","最大")   
+
+x.showInfo()
+y.showInfo()
+z.showInfo()
